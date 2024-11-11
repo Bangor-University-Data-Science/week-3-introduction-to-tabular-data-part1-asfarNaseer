3fd3883cfd216b8e1f4446d3f659e5dfdfabf5b3
asfarNaseer <98314579+asfarNaseer@users.noreply.github.com>
2024-11-11 00:00:42 +0000

    lab 3

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,7 +1,11 @@
+
+import os
 import pandas as pd
 from titanic_analysis.data_loader import load_titanic_data
 
 def test_load_titanic_data():
-    df = load_titanic_data("../../data/titanic.csv")
+    # Ensure the path matches your project structure
+    filepath = os.path.join("data", "titanic.csv")
+    df = load_titanic_data(filepath)
     assert isinstance(df, pd.DataFrame), "The returned object should be a DataFrame"
     assert not df.empty, "The DataFrame should not be empty"
